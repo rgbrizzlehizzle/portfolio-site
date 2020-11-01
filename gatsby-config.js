71a167c75cc0810774cc74491167699362331ce1
Ryan Brink <ryanbrink@protonmail.com>
2020-11-01 18:23:38 -0500

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: 'Ryan Brink',
+    author: 'Ryan Brink',
+    description: 'Portfolio website for Ryan Brink'
+  }
 }
