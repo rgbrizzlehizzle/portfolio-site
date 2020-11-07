0fc3c500ce8896aebb9042045234e03f6a5d2d2b
Ryan Brink <ryanbrink@protonmail.com>
2020-11-07 13:48:39 -0500

    me likey

--- src/components/Console.tsx
+++ src/components/Console.tsx
@@ -1,14 +1,15 @@
 import React, { FunctionComponent } from "react"
 import Terminal from "terminal-in-react"
 // @ts-ignore
-import pseudoFileSystemPlugin from 'terminal-in-react-pseudo-file-system-plugin';
+import pseudoFileSystemPlugin from "terminal-in-react-pseudo-file-system-plugin"
 
 export const Console: FunctionComponent = () => {
-  const FileSystemPlugin = pseudoFileSystemPlugin();
+  const FileSystemPlugin = pseudoFileSystemPlugin()
 
   return (
     <Terminal
       msg='“The purpose of a storyteller is not to tell you how to think, but to give you questions to think upon.”'
+      commandPassThrough={() => `Invalid command, type "help" to view valid commands`}
       color='#ebdbb2'
       backgroundColor='#282828'
       prompt='#d65d0e'
@@ -16,14 +17,19 @@ export const Console: FunctionComponent = () => {
       hideTopBar={true}
       allowTabs={false}
       style={{ fontWeight: "bold", fontSize: "1.1em" }}
-      commands={{ }}
+      commands={{}}
       // @ts-ignore
       descriptions={{
-        show: 'potato',
+        show: false,
         color: false
       }}
       plugins={[
-        FileSystemPlugin
+        {
+          class: FileSystemPlugin,
+          config: {
+
+          }
+        }
       ]}
     />
   )
